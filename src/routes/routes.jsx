2776695db2471fa87635994d86d7b38670a8e05b
nahidswe <nahidswe@gmail.com>
2023-11-04 01:43:09 +0600

    city comparison based on aqi index added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
+import CityComparison from "../pages/CityComparison";
 
 function PageRoutes() {
 
@@ -11,6 +12,9 @@ function PageRoutes() {
                 <Routes>
                     <Route path="/" element={
                         <HomePage />
+                    } />
+                    <Route path="/air-quality-comparison" element={
+                        <CityComparison />
                     } />
 
                     <Route path="/*" element={<ErrorPage />} />
